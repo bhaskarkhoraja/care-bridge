87e1a21a57980146f383b214ff68adc640e36f51
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-02-03 15:54:58 +0545

    feat: otp and verified at

--- apps/server/migrations/20240126150418_create_contacts_table.sql
+++ apps/server/migrations/20240126150418_create_contacts_table.sql
@@ -4,6 +4,8 @@ CREATE TABLE IF NOT EXISTS contact (
   number TEXT NOT NULL,
   prefix TEXT NOT NULL,
   type TEXT CHECK (type IN ('Telephone', 'Mobile')),
+  phone_otp INT,
+  phone_verified_at TIMESTAMPTZ,
   profile_id UUID REFERENCES profile(id),
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
