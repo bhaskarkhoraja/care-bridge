00eea64f035f8df729672dd932f3f0a8956bc5a0
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-03-07 21:35:00 +0545

    refactor: change client to web-env

--- apps/web/src/lib/env.mjs
+++ apps/web/src/lib/env.mjs
@@ -1,7 +1,7 @@
 import { createEnv } from "@t3-oss/env-nextjs"
 import { z } from "zod"
 
-export const clientEnv = createEnv({
+export const webEnv = createEnv({
   server: {
     WEB_URL: z.string().url(),
     SERVER_URL: z.string().url(),
