57e9f76f167a5879c8d379b5f764945155c0ae3a
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-03-14 08:10:14 +0545

    style: change skeleton shimmer effect

--- apps/web/src/components/ui/skeleton.tsx
+++ apps/web/src/components/ui/skeleton.tsx
@@ -7,7 +7,7 @@ function Skeleton({
   return (
     <div
       className={cn(
-        "before:border-primary/10 before:via-primary/15 relative space-y-5 overflow-hidden before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:border-t before:bg-gradient-to-r before:from-transparent before:to-transparent",
+        "before:border-primary/10 before:via-primary/15 relative space-y-5 overflow-hidden rounded-[inherit] before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:border-t before:bg-gradient-to-r before:from-transparent before:to-transparent",
         className
       )}
       {...props}
