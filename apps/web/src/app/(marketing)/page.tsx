74d0a75ea8884062d950ad1369b2f2003245dbb3
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-03-15 20:39:49 +0545

    feat: page for marketing

---
+++ apps/web/src/app/(marketing)/page.tsx
@@ -0,0 +1,13 @@
+/* import { trpc } from "@web/src/app/trpc" */
+
+/* import ClientSide from "../components/ClientSide" */
+
+export default async function Home() {
+  /* const { greeting } = await trpc.hello.query({ name: `Tom` }) */
+  return (
+    <div>
+      {/* {greeting} <ClientSide /> */}Beautifully designed components that you
+      can copy and paste into your apps. Accessible. Customizable. Open Source.
+    </div>
+  )
+}
--- apps/web/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { trpc } from "@web/src/app/trpc"
-
-import ClientSide from "../components/ClientSide"
-
-export default async function Home() {
-  /* const { greeting } = await trpc.hello.query({ name: `Tom` }) */
-  return <div>{/* {greeting} <ClientSide /> */}</div>
-}
