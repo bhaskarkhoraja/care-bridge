6a421bfe3ffbdd87911a04e51ea2fc54d440c010
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-03-16 13:26:19 +0545

    feat: setup single .env

--- apps/server/src/lib/env.validation.ts
+++ apps/server/src/lib/env.validation.ts
@@ -5,27 +5,21 @@ import { z } from 'zod';
 
 export const serverEnvSchema = z.object({
   DATABASE_URL: z.string(),
-  APP_URL: z.string().url(),
-  API_URL: z.string().url(),
-  APP_PORT: z.coerce.number().positive(),
-  API_PORT: z.coerce.number().positive(),
+  NEXT_PUBLIC_WEB_URL: z.string().url(),
+  SERVER_URL: z.string().url(),
 });
 
 export let serverEnv: z.infer<typeof serverEnvSchema> = {
   DATABASE_URL: '',
-  APP_URL: '',
-  API_URL: '',
-  APP_PORT: 0,
-  API_PORT: 0,
+  NEXT_PUBLIC_WEB_URL: '',
+  SERVER_URL: '',
 };
 
 export function createEnv(config: Record<string, any>) {
   const env = {
     DATABASE_URL: config.DATABASE_URL,
-    APP_URL: config.APP_URL,
-    API_URL: config.API_URL,
-    APP_PORT: config.APP_PORT,
-    API_PORT: config.API_PORT,
+    NEXT_PUBLIC_WEB_URL: config.NEXT_PUBLIC_WEB_URL,
+    SERVER_URL: config.SERVER_URL,
   };
 
   const validatedConfig = serverEnvSchema.safeParse(env);
--- apps/web/src/lib/env.mjs
+++ apps/web/src/lib/env.mjs
@@ -4,6 +4,12 @@ import { z } from "zod"
 export const webEnv = createEnv({
   server: {
     SERVER_URL: z.string().url(),
+    NEXTAUTH_URL: z.string().url().optional(),
+    NEXTAUTH_SECRET: z.string().min(1),
+    GITHUB_CLIENT_ID: z.string().min(1),
+    GITHUB_CLIENT_SECRET: z.string().min(1),
+    GOOGLE_CLIENT_ID: z.string().min(1),
+    GOOGLE_CLIENT_SECRET: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_WEB_URL: z.string().url(),
@@ -11,5 +17,12 @@ export const webEnv = createEnv({
   runtimeEnv: {
     SERVER_URL: process.env.SERVER_URL,
     NEXT_PUBLIC_WEB_URL: process.env.NEXT_PUBLIC_WEB_URL,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
   },
+  emptyStringAsUndefined: true,
 })
