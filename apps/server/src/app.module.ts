b818766ae468292ca4a6266650e810882c73faba
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-03-16 20:31:27 +0545

    chore: remove trpc, switching to ts-rest

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { TrpcModule } from './trpc/trpc.module'
 import { createEnv } from '@server/lib/env.validation'
 import { ConfigModule } from '@nestjs/config'
 
 @Module({
   imports: [
-    TrpcModule,
     ConfigModule.forRoot({
       validate: createEnv,
     }),
--- apps/web/src/app/(marketing)/page.tsx
+++ apps/web/src/app/(marketing)/page.tsx
@@ -1,8 +1,3 @@
-/* import { trpc } from "@web/src/app/trpc" */
-
-/* import ClientSide from "../components/ClientSide" */
-
 export default async function Home() {
-  /* const { greeting } = await trpc.hello.query({ name: `Tom` }) */
-  return <div>{/* {greeting} <ClientSide /> */}</div>
+  return <div></div>
 }
