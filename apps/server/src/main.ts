3c47f4aea9eb44e2759c830f01fd25a3cd9dd508
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-03-16 14:20:14 +0545

    feat: only allow cross origin in web url

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TrpcRouter } from './trpc/trpc.router';
+import { serverEnv } from './lib/env.validation';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({ origin: [serverEnv.NEXT_PUBLIC_WEB_URL] });
   const trpc = app.get(TrpcRouter);
   trpc.applyMiddleware(app);
   await app.listen(4000);
