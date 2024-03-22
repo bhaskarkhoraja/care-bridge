c681199c55912a9ca93bb8c9d3b4d34399a6dce6
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-03-22 17:09:08 +0545

    feat: add postgres module for nest

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common'
 import { createEnv } from '@server/lib/env.validation'
+import { createEnv, serverEnv } from '@server/lib/env.validation'
 import { ConfigModule } from '@nestjs/config'
+import { PostgresModule } from 'nest-postgres'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       validate: createEnv,
+    }),
+    PostgresModule.forRoot({
+      connectionString: serverEnv.DATABASE_URL,
     }),
   ],
   controllers: [],
