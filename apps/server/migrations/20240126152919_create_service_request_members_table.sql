d452d4f826343a7812d80a9571fc794371a03e79
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-03-23 21:43:59 +0545

    chore: remove old database schema

--- apps/server/migrations/20240126152919_create_service_request_members_table.sql
+++
@@ -1,8 +0,0 @@
--- table to store family member in specific service request
-CREATE TABLE IF NOT EXISTS service_request (
-  id UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID(),
-  profile_id UUID REFERENCES profile(id),
-  service_request_id UUID REFERENCES service_request(id),
-  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-);
--- apps/server/migrations/20240128043406_create_old_messages_table.sql
+++
@@ -1,8 +0,0 @@
--- table to store messages
-CREATE TABLE IF NOT EXISTS old_message (
-  id UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID(),
-  message_id UUID REFERENCES message(id),
-  message TEXT NOT NULL,
-  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-);
