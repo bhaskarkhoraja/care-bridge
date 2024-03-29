833637ee2b52001a9bd729abbdadada485cbdbb1
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-03-29 19:08:35 +0545

    feat: use cookie parser

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { serverEnv } from './lib/env.validation'
+import * as cookieParser from 'cookie-parser'
 import '../../../types/lib/next-auth'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors({ origin: [serverEnv.NEXT_PUBLIC_WEB_URL] })
+  app.use(cookieParser())
   await app.listen(4000)
 }
 bootstrap()
