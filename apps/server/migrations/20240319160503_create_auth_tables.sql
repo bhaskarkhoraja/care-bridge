3a441c54fd37ce3bf65f3e4b80c01b9a79cb5428
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-03-30 20:46:22 +0545

    docs: description of tables and columns

--- apps/server/migrations/20240319160503_create_auth_tables.sql
+++ apps/server/migrations/20240319160503_create_auth_tables.sql
@@ -49,3 +49,43 @@ CREATE TABLE users
 
   PRIMARY KEY (id)
 );
+
+-- descriptions
+COMMENT ON TABLE verification_token IS 'Verification token for magic links';
+COMMENT ON COLUMN verification_token.identifier IS 'User email associated with the verification token';
+COMMENT ON COLUMN verification_token.expires IS 'Expiry timestamp of the verification token';
+COMMENT ON COLUMN verification_token.token IS 'Verification token value';
+
+
+-- descriptions
+COMMENT ON TABLE accounts IS 'Table to store user accounts and associated tokens';
+COMMENT ON COLUMN accounts.id IS 'Unique identifier for the account';
+COMMENT ON COLUMN accounts."userId" IS 'User ID associated with the account';
+COMMENT ON COLUMN accounts.type IS 'Type of the account';
+COMMENT ON COLUMN accounts.provider IS 'Provider of the account e.g, google, github';
+COMMENT ON COLUMN accounts."providerAccountId" IS 'Provider account ID';
+COMMENT ON COLUMN accounts.refresh_token IS 'Refresh token for the account';
+COMMENT ON COLUMN accounts.access_token IS 'Access token for the account';
+COMMENT ON COLUMN accounts.expires_at IS 'Expiry timestamp for the account';
+COMMENT ON COLUMN accounts.id_token IS 'ID token for the account';
+COMMENT ON COLUMN accounts.scope IS 'Scope of the account';
+COMMENT ON COLUMN accounts.session_state IS 'Session state of the account';
+COMMENT ON COLUMN accounts.token_type IS 'Type of token for the account';
+
+
+-- descriptions
+COMMENT ON TABLE sessions IS 'Table to store user sessions';
+COMMENT ON COLUMN sessions.id IS 'Unique identifier for the session';
+COMMENT ON COLUMN sessions."userId" IS 'User ID associated with the session';
+COMMENT ON COLUMN sessions.expires IS 'Expiry timestamp of the session';
+COMMENT ON COLUMN sessions."sessionToken" IS 'Session token value';
+
+-- descriptions
+COMMENT ON TABLE users IS 'Table to store user information';
+COMMENT ON COLUMN users.id IS 'Unique identifier for the user';
+COMMENT ON COLUMN users.name IS 'Name of the user';
+COMMENT ON COLUMN users.email IS 'Email address of the user';
+COMMENT ON COLUMN users."emailVerified" IS 'Timestamp of email verification';
+COMMENT ON COLUMN users.image IS 'URL of the user image';
+COMMENT ON COLUMN users.role IS 'Role (admin, user)';
+COMMENT ON COLUMN users.completed_profile IS 'Indicates if the user has completed their profile';
