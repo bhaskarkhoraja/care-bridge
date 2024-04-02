9ec741679283696e2e6cd648c0781ff2a8a1a80c
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-04-02 09:16:01 +0545

    feat: absolute url func

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -1,7 +1,16 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+import { webEnv } from "./env"
+
 /* cn function for organizing class */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+/**
+ * return absolute url
+ **/
+export function absoluteUrl(path: string) {
+  return `${webEnv.NEXT_PUBLIC_WEB_URL}${path}`
 }
