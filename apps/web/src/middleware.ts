603f7b59b0ef505842d759fab348c926770fe3bf
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-04-02 22:54:59 +0545

    fix: error during build time due to edge

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,18 +1,49 @@
+import { cookies } from "next/headers"
 import { NextResponse } from "next/server"
+import { format } from "@web/src/lib/utils"
+import { AdapterSession, AdapterUser } from "next-auth/adapters"
 import { withAuth } from "next-auth/middleware"
-import { getSession } from "next-auth/react"
+
+import { webEnv } from "./lib/env"
 
 export default withAuth(
   async function middleware(req) {
-    // get Sessions
-    const session = await getSession({
-      req: {
-        ...req,
-        headers: {
-          ...Object.fromEntries(req.headers),
-        },
-      },
-    })
+    // get cookies session token
+    const sessionToken = cookies().get("next-auth.session-token")?.value
+
+    // stores session and user object
+    let session = null
+    let user = null
+
+    try {
+      /* if sessionToken is not available throw error */
+      if (!sessionToken) throw Error
+
+      /* if its available check if its valid or not */
+      const response = await fetch(
+        `${webEnv.SERVER_URL}/auth/session/${sessionToken}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "x-auth-secret": webEnv.NEXTAUTH_SECRET,
+          },
+        }
+      ).then((response) => {
+        if (!response.ok) {
+          throw Error
+        }
+        return response.json()
+      })
+
+      if (!response) {
+        throw Error
+      }
+      session = format<AdapterSession>(response.session)
+      user = format<AdapterUser>(response.user)
+    } catch {
+      // do nothing if fails it means the user is not logged in
+    }
 
     // check if its auth page
     const isAuthPage = req.nextUrl.pathname.startsWith("/auth")
@@ -25,7 +56,7 @@ export default withAuth(
     }
 
     // check if token is available if not redirect to login page
-    if (!session) {
+    if (!session || !user) {
       let from = req.nextUrl.pathname
       if (req.nextUrl.search) {
         from += req.nextUrl.search
@@ -39,27 +70,23 @@ export default withAuth(
     // check if its admin page if yes check if the user is admin
     const isAdminPage = req.nextUrl.pathname.startsWith("/admin")
     if (isAdminPage) {
-      if (session.user.role != "admin") {
+      if (user.role != "admin") {
         return NextResponse.redirect(new URL(`/user`, req.url))
       }
     }
 
     // check if profile is completed
-    if (!session.user.completed_profile) {
+    const isCompleteProfilePage =
+      req.nextUrl.pathname === "/user/profile/complete-profile"
+    if (!user.completed_profile) {
       return NextResponse.redirect(
         new URL("/user/profile/complete-profile", req.url)
       )
     }
 
-    // check if user is in complete-profile page and redirect to user if has already completed-profile
-    const isCompleteProfilePage =
-      req.nextUrl.pathname === "/user/profile/complete-profile"
-    if (isCompleteProfilePage) {
-      if (session.user.completed_profile) {
-        return NextResponse.redirect(new URL("/user", req.url))
-      }
-
-      return null
+    // check if profile is completed but still in complete-profile page
+    if (isCompleteProfilePage && user.completed_profile) {
+      return NextResponse.redirect(new URL(`/user`, req.url))
     }
   },
   {
