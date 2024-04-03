d77ef175ae8008e2e7fd5473228be786fb3cfd1a
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-04-03 21:38:27 +0545

    feat: show user account nav if loggedin

--- apps/web/src/components/layout/site-header.tsx
+++ apps/web/src/components/layout/site-header.tsx
@@ -3,42 +3,56 @@ import MainNav from "@web/src/components/layout/main-nav"
 import MobileNav from "@web/src/components/layout/mobile-nav"
 import ModeToggle from "@web/src/components/theme/mode-toggle"
 import { buttonVariants } from "@web/src/components/ui/button"
+import { UserAccountNav } from "@web/src/components/user-account-nav"
+import { getCurrentUser } from "@web/src/lib/session"
 import { cn } from "@web/src/lib/utils"
 
-const SiteHeader = () => {
+const SiteHeader = async () => {
+  const user = await getCurrentUser()
+
   return (
     <header className="border-border/40 bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 w-full border-b backdrop-blur">
       <div className="container flex h-14 items-center justify-between">
         <MainNav />
         <MobileNav />
-        <div className="flex items-center justify-between space-x-2 md:justify-end">
-          <nav className="flex items-center gap-4">
-            <ModeToggle type="button" />
-            <div
-              aria-description="login signup container"
-              className="space-x-2"
-            >
-              <Link
-                href="/auth/login"
-                className={cn(
-                  buttonVariants({ variant: "default", size: "sm" }),
-                  "px-4"
-                )}
+        {user ? (
+          <UserAccountNav
+            user={{
+              name: user.name,
+              image: user.image,
+              email: user.email,
+            }}
+          />
+        ) : (
+          <div className="flex items-center justify-between space-x-2 md:justify-end">
+            <nav className="flex items-center gap-4">
+              <ModeToggle type="button" />
+              <div
+                aria-description="login signup container"
+                className="space-x-2"
               >
-                Login
-              </Link>
-              <Link
-                href="/auth/signup"
-                className={cn(
-                  buttonVariants({ variant: "ghost", size: "sm" }),
-                  "px-4"
-                )}
-              >
-                Signup
-              </Link>
-            </div>
-          </nav>
-        </div>
+                <Link
+                  href="/auth/login"
+                  className={cn(
+                    buttonVariants({ variant: "default", size: "sm" }),
+                    "px-4"
+                  )}
+                >
+                  Login
+                </Link>
+                <Link
+                  href="/auth/signup"
+                  className={cn(
+                    buttonVariants({ variant: "ghost", size: "sm" }),
+                    "px-4"
+                  )}
+                >
+                  Signup
+                </Link>
+              </div>
+            </nav>
+          </div>
+        )}
       </div>
     </header>
   )
