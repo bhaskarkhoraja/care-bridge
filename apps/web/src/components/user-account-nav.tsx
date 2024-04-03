ad423aba29a92efca4f7eb98e8ba26313ed885be
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-04-03 21:37:56 +0545

    feat: user-account-nav component

---
+++ apps/web/src/components/user-account-nav.tsx
@@ -0,0 +1,86 @@
+"use client"
+
+import Link from "next/link"
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@web/src/components/ui/avatar"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@web/src/components/ui/dropdown-menu"
+import { User } from "next-auth"
+import { signOut } from "next-auth/react"
+
+interface UserAccountNavProps extends React.HTMLAttributes<HTMLDivElement> {
+  user: Pick<User, "name" | "image" | "email">
+}
+
+/**
+ * Get user initials, Bhaskar Khoraja -> BK
+ **/
+function getShortName(name: string): string {
+  let names = name.split(" ")
+  let shortName = ""
+
+  if (names.length === 1) {
+    shortName = names[0].charAt(0).toUpperCase()
+  } else {
+    shortName =
+      names[0].charAt(0).toUpperCase() +
+      names[names.length - 1].charAt(0).toUpperCase()
+  }
+
+  return shortName
+}
+
+export const UserAccountNav = ({ user }: UserAccountNavProps) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src={user.image as string} alt={"hello"} />
+          <AvatarFallback>{getShortName(user.name as string)}</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <div className="flex items-center justify-start gap-2 p-2">
+          <div className="flex flex-col space-y-1 leading-none">
+            {user.name && <p className="font-medium">{user.name}</p>}
+            {user.email && (
+              <p className="text-muted-foreground w-[200px] truncate text-sm">
+                {user.email}
+              </p>
+            )}
+          </div>
+        </div>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <Link href="/dashboard">Dashboard</Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem asChild>
+          <Link href="/dashboard/billing">Billing</Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem asChild>
+          <Link href="/dashboard/settings">Settings</Link>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          className="cursor-pointer"
+          onSelect={(event) => {
+            event.preventDefault()
+            signOut({
+              callbackUrl: `${window.location.origin}/login`,
+            })
+          }}
+        >
+          Sign out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
