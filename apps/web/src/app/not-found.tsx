c080c646454b1fac722c68c698923e5d2404cd75
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-04-09 21:45:05 +0545

    feat: navigate to previous page instead of home

--- apps/web/src/app/not-found.tsx
+++ apps/web/src/app/not-found.tsx
@@ -1,23 +1,13 @@
-import Link from "next/link"
-import { getServerSession } from "next-auth"
-
-import { buttonVariants } from "../components/ui/button"
-import { authOptions } from "../lib/auth"
+import BackButton from "../components/layout/back-button"
 
 export default async function NotFound() {
-  const session = await getServerSession(authOptions)
-  const path = session?.user ? "/user" : "/"
-
   return (
-    <main className="flex h-screen w-screen flex-col items-center justify-center">
+    <main className="container relative flex h-screen w-screen flex-col items-center justify-center">
+      <div className="container absolute top-8">
+        <BackButton />
+      </div>
       <h1 className="text-5xl font-black md:text-7xl">404</h1>
       <p className="text-sm md:text-lg">Page not found</p>
-      <Link
-        href={path}
-        className={`mt-6 ${buttonVariants({ variant: "default", size: "sm" })}`}
-      >
-        Return Home
-      </Link>
     </main>
   )
 }
