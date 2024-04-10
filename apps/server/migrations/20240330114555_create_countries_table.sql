3eedfad8d68af55b00c92a9d8d26d0acab852858
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-04-10 22:52:42 +0545

    chore: remove iso_code

--- apps/server/migrations/20240330114555_create_countries_table.sql
+++ apps/server/migrations/20240330114555_create_countries_table.sql
@@ -4,7 +4,6 @@ CREATE TABLE IF NOT EXISTS country (
   name VARCHAR(255) NOT NULL,
   nationality VARCHAR(255) NOT NULL,
   short_name VARCHAR(2) NOT NULL,
-  iso_code VARCHAR(3) NOT NULL,
   phone_code VARCHAR(5) NOT NULL,
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
@@ -16,7 +15,6 @@ COMMENT ON COLUMN country.id IS 'Unique identifier for the country';
 COMMENT ON COLUMN country.name IS 'Full name of the country';
 COMMENT ON COLUMN country.nationality IS 'Nationality associated with the country';
 COMMENT ON COLUMN country.short_name IS 'Short name or code for the country';
-COMMENT ON COLUMN country.iso_code IS 'ISO code of the country';
 COMMENT ON COLUMN country.phone_code IS 'Phone code for the country';
 COMMENT ON COLUMN country.created_at IS 'Timestamp of when the country record was created';
 COMMENT ON COLUMN country.updated_at IS 'Timestamp of the last update to the country record';
