1ff22f86f530617cb607eb2473dc59821c6b8f7b
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-04-10 21:45:04 +0545

    chore: remove logo from footer

--- apps/web/src/components/layout/site-footer.tsx
+++ apps/web/src/components/layout/site-footer.tsx
@@ -1,6 +1,5 @@
 import * as React from "react"
 import Link from "next/link"
-import { Icons } from "@web/src/components/icons"
 import { siteConfig } from "@web/src/config/site"
 import { cn } from "@web/src/lib/utils"
 
@@ -9,7 +8,6 @@ const SiteFooter = ({ className }: React.HTMLAttributes<HTMLElement>) => {
     <footer className={cn(className)}>
       <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          <Icons.logoshort className="size-8" />
           <p className="text-center text-sm leading-loose md:text-left">
             Built by{" "}
             <Link
