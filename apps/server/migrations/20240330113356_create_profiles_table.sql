976d4062992d5b1d568c1b1d8e8aeb1a7b055186
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-04-10 22:08:45 +0545

    chore: change column name profile_url

--- apps/server/migrations/20240330113356_create_profiles_table.sql
+++ apps/server/migrations/20240330113356_create_profiles_table.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS profile (
   gender user_gender,
   date_of_birth TIMESTAMPTZ NOT NULL,
   active_status BOOLEAN NOT NULL,
-  image_url TEXT,
+  profile_url TEXT,
   balance_exhausted INT NOT NULL DEFAULT 0,
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
@@ -26,7 +26,7 @@ COMMENT ON COLUMN profile.user_name IS 'User chosen username';
 COMMENT ON COLUMN profile.gender IS 'gender (male, female, others)';
 COMMENT ON COLUMN profile.date_of_birth IS 'Date of birth of the user';
 COMMENT ON COLUMN profile.active_status IS 'Indicates if user is active';
-COMMENT ON COLUMN profile.image_url IS 'URL pointing to the user''s profile image';
+COMMENT ON COLUMN profile.profile_url IS 'URL pointing to the user''s profile image';
 COMMENT ON COLUMN profile.balance_exhausted IS 'Tracks the amount of balance exhausted';
 COMMENT ON COLUMN profile.created_at IS 'Timestamp of when the profile was created';
 COMMENT ON COLUMN profile.updated_at IS 'Timestamp of the last update to the profile';
