e68864805671656d1edc0ac13de47a90f929db26
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-04-16 07:29:11 +0545

    refactor: remome user_contact type

--- apps/server/migrations/20240330120314_create_contacts_table.sql
+++ apps/server/migrations/20240330120314_create_contacts_table.sql
@@ -1,11 +1,8 @@
 -- table to store contact
-CREATE TYPE user_contact AS ENUM ('tel', 'mob');
-
 CREATE TABLE IF NOT EXISTS contact (
   id UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID(),
   number INT NOT NULL,
   prefix VARCHAR(5) NOT NULL,
-  type user_contact,
   phone_verified_at TIMESTAMPTZ,
   profile_id UUID REFERENCES profile(id),
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
@@ -16,8 +13,6 @@ CREATE TABLE IF NOT EXISTS contact (
 COMMENT ON TABLE contact IS 'Contact information for a profile';
 COMMENT ON COLUMN contact.id IS 'Unique identifier for the contact';
 COMMENT ON COLUMN contact.number IS 'Contact number';
-COMMENT ON COLUMN contact.prefix IS 'Prefix or country code for the contact number';
-COMMENT ON COLUMN contact.type IS 'Contact (tel, mobile)';
 COMMENT ON COLUMN contact.phone_verified_at IS 'Timestamp of when the phone number was verified';
 COMMENT ON COLUMN contact.profile_id IS 'Foreign key referencing the profile to which the contact belongs';
 COMMENT ON COLUMN contact.created_at IS 'Timestamp of when the contact was created';
