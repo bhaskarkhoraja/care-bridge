644f4845ab54a825e6f7b8cf3cf430296b9bdb44
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-04-16 07:37:46 +0545

    feat: stepper component

---
+++ apps/web/src/components/stepper-form-action.tsx
@@ -0,0 +1,40 @@
+"ues client"
+
+import { Button } from "@web/src/components/ui/button"
+import { useStepper } from "@web/src/components/ui/stepper"
+
+export const StepperFormActions = () => {
+  const {
+    prevStep,
+    resetSteps,
+    isDisabledStep,
+    hasCompletedAllSteps,
+    isLastStep,
+    isOptionalStep,
+  } = useStepper()
+
+  return (
+    <div className="mt-4 flex w-full justify-end gap-2">
+      {hasCompletedAllSteps ? (
+        <Button size="sm" onClick={resetSteps} type="button">
+          Reset
+        </Button>
+      ) : (
+        <>
+          <Button
+            disabled={isDisabledStep}
+            onClick={prevStep}
+            size="sm"
+            variant="ghost"
+            type="button"
+          >
+            Prev
+          </Button>
+          <Button size="sm">
+            {isLastStep ? "Finish" : isOptionalStep ? "Skip" : "Next"}
+          </Button>
+        </>
+      )}
+    </div>
+  )
+}
