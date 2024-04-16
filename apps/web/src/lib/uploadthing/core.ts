6ca3a140cead8d116532c1a1eb2e67e8e62f3b71
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-04-16 07:38:58 +0545

    feat: max and min 1 file with cleanup console

--- apps/web/src/lib/uploadthing/core.ts
+++ apps/web/src/lib/uploadthing/core.ts
@@ -8,7 +8,9 @@ const f = createUploadthing()
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
-  imageUploader: f({ image: { maxFileSize: "4MB" } })
+  documentUploader: f({
+    image: { maxFileSize: "4MB", maxFileCount: 1, minFileCount: 1 },
+  })
     // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
       // This code runs on your server before upload
@@ -20,12 +22,7 @@ export const ourFileRouter = {
       // Whatever is returned here is accessible in onUploadComplete as `metadata`
       return { userId: session.user.id }
     })
-    .onUploadComplete(async ({ metadata, file }) => {
-      // This code RUNS ON YOUR SERVER after upload
-      console.log("Upload complete for userId:", metadata.userId)
-
-      console.log("file url", file.url)
-
+    .onUploadComplete(async ({ metadata }) => {
       // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
       return { uploadedBy: metadata.userId }
     }),
