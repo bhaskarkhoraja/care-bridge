0c503c68fd1c02fd73a93d39c6587ee01b926f57
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-04-17 08:06:26 +0545

    feat: wrap with form

--- apps/web/src/app/(account)/_components/document-form.tsx
+++ apps/web/src/app/(account)/_components/document-form.tsx
@@ -1,14 +1,22 @@
 "use client"
 
-import { useState } from "react"
 import Image from "next/image"
+import { zodResolver } from "@hookform/resolvers/zod"
 import { StepperFormActions } from "@web/src/components/stepper-form-action"
 import { buttonVariants } from "@web/src/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@web/src/components/ui/form"
 import { Label } from "@web/src/components/ui/label"
 import { useStepper } from "@web/src/components/ui/stepper"
 import { UploadDropzone } from "@web/src/lib/uploadthing/uploadthing"
 import { cn } from "@web/src/lib/utils"
 import { UploadCloud } from "lucide-react"
+import { useForm } from "react-hook-form"
 import { toast } from "sonner"
 import { z } from "zod"
 
@@ -17,116 +25,146 @@ interface DocumentFormProps {
 }
 
 const DocumentFormSchema = z.object({
-  profileUrl: z.string().url(),
-  documentUrl: z.string().url(),
+  profileUrl: z.string().url({ message: "Profile image is required." }),
+  documentUrl: z.string().url({ message: "Document image is required." }),
 })
 
 // Uploads the images to uploadthing and stores url
 const DocumentForm: React.FC<DocumentFormProps> = ({}) => {
   const { nextStep } = useStepper()
 
-  const [imageUrl, setImageUrl] = useState<z.infer<typeof DocumentFormSchema>>({
-    profileUrl: "",
-    documentUrl: "",
+  const form = useForm<z.infer<typeof DocumentFormSchema>>({
+    resolver: zodResolver(DocumentFormSchema),
+    defaultValues: {
+      profileUrl: "",
+      documentUrl: "",
+    },
   })
 
-  return (
-    <div>
-      <div className="mt-8 flex flex-col items-center justify-center gap-4 md:flex-row">
-        <div className="flex flex-col items-center justify-center">
-          <Label className="w-full text-center">Profile Picture</Label>
-          <UploadDropzone
-            appearance={{
-              container: "cursor-pointer size-56",
-              label: cn(
-                "text-foreground hover:text-foreground",
-                imageUrl.profileUrl && "hidden"
-              ),
-              allowedContent: cn(
-                "text-foreground/70",
-                imageUrl.profileUrl && "hidden"
-              ),
-              button: cn(
-                buttonVariants({ variant: "default", size: "sm" }),
-                "after:bg-primary data-[state=uploading]:bg-primary/70 disabled:opacity-100"
-              ),
-            }}
-            content={{
-              uploadIcon() {
-                if (imageUrl.profileUrl) {
-                  return (
-                    <Image
-                      src={imageUrl.profileUrl}
-                      width={200}
-                      height={200}
-                      className="h-24 w-auto rounded-md"
-                      alt="Profile Image"
-                    />
-                  )
-                }
-                return <UploadCloud className="size-12" />
-              },
-            }}
-            endpoint="documentUploader"
-            onClientUploadComplete={(res) => {
-              setImageUrl({ ...imageUrl, profileUrl: res[0].url })
-              toast.success("Document uploaded successful")
-            }}
-            onUploadError={() => {
-              toast.error("Document upload failed!")
-            }}
-          />
-        </div>
+  const onSubmit = (data: z.infer<typeof DocumentFormSchema>) => {
+    /* nextStep() */
+  }
 
-        <div className="flex flex-col items-center justify-center">
-          <Label className="w-full text-center">
-            Passport / Citizenship / License
-          </Label>
-          <UploadDropzone
-            appearance={{
-              container: "cursor-pointer size-56",
-              label: cn(
-                "text-foreground hover:text-foreground",
-                imageUrl.documentUrl && "hidden"
-              ),
-              allowedContent: cn(
-                "text-foreground/70",
-                imageUrl.documentUrl && "hidden"
-              ),
-              button: cn(
-                buttonVariants({ variant: "default", size: "sm" }),
-                "after:bg-primary data-[state=uploading]:bg-primary/70 disabled:opacity-100"
-              ),
-            }}
-            content={{
-              uploadIcon() {
-                if (imageUrl.documentUrl) {
-                  return (
-                    <Image
-                      src={imageUrl.documentUrl}
-                      width={200}
-                      height={200}
-                      className="h-20 w-auto rounded-md"
-                      alt="Profile Image"
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <div className="mt-8 flex flex-col items-center justify-center gap-4 md:flex-row">
+          <FormField
+            control={form.control}
+            name="profileUrl"
+            render={({ field }) => (
+              <FormItem>
+                <div className="flex flex-col items-center justify-center">
+                  <Label className="w-full text-center">Profile Picture</Label>
+                  <FormControl>
+                    <UploadDropzone
+                      appearance={{
+                        container: "cursor-pointer size-56",
+                        label: cn(
+                          "text-foreground hover:text-foreground",
+                          field.value && "hidden"
+                        ),
+                        allowedContent: cn(
+                          "text-foreground/70",
+                          field.value && "hidden"
+                        ),
+                        button: cn(
+                          buttonVariants({ variant: "default", size: "sm" }),
+                          "after:bg-primary data-[state=uploading]:bg-primary/70 disabled:opacity-100"
+                        ),
+                      }}
+                      content={{
+                        uploadIcon() {
+                          if (field.value) {
+                            return (
+                              <Image
+                                src={field.value}
+                                width={200}
+                                height={200}
+                                className="h-24 w-auto rounded-md"
+                                alt="Profile Image"
+                              />
+                            )
+                          }
+                          return <UploadCloud className="size-12" />
+                        },
+                      }}
+                      endpoint="documentUploader"
+                      onClientUploadComplete={(res) => {
+                        field.onChange(res[0].url)
+                        toast.success("Profile uploaded successful")
+                      }}
+                      onUploadError={() => {
+                        toast.error("Profile upload failed!")
+                      }}
                     />
-                  )
-                }
-                return <UploadCloud className="size-12" />
-              },
-            }}
-            endpoint="documentUploader"
-            onClientUploadComplete={(res) => {
-              setImageUrl({ ...imageUrl, documentUrl: res[0].url })
-              toast.success("Document uploaded successful")
-            }}
-            onUploadError={() => {
-              toast.error("Document upload failed!")
-            }}
+                  </FormControl>
+                </div>
+                <FormMessage className="text-center" />
+              </FormItem>
+            )}
           />
+          <FormField
+            control={form.control}
+            name="profileUrl"
+            render={({ field }) => (
+              <FormItem>
+                <div className="flex flex-col items-center justify-center">
+                  <Label className="w-full text-center">
+                    Passport / Citizenship / License
+                  </Label>
+                  <FormControl>
+                    <UploadDropzone
+                      appearance={{
+                        container: "cursor-pointer size-56",
+                        label: cn(
+                          "text-foreground hover:text-foreground",
+                          field.value && "hidden"
+                        ),
+                        allowedContent: cn(
+                          "text-foreground/70",
+                          field.value && "hidden"
+                        ),
+                        button: cn(
+                          buttonVariants({ variant: "default", size: "sm" }),
+                          "after:bg-primary data-[state=uploading]:bg-primary/70 disabled:opacity-100"
+                        ),
+                      }}
+                      content={{
+                        uploadIcon() {
+                          if (field.value) {
+                            return (
+                              <Image
+                                src={field.value}
+                                width={200}
+                                height={200}
+                                className="h-24 w-auto rounded-md"
+                                alt="Profile Image"
+                              />
+                            )
+                          }
+                          return <UploadCloud className="size-12" />
+                        },
+                      }}
+                      endpoint="documentUploader"
+                      onClientUploadComplete={(res) => {
+                        field.onChange(res[0].url)
+                        toast.success("Document uploaded successful")
+                      }}
+                      onUploadError={() => {
+                        toast.error("Document upload failed!")
+                      }}
+                    />
+                  </FormControl>
+                </div>
+                <FormMessage className="text-center" />
+              </FormItem>
+            )}
+          />{" "}
         </div>
-      </div>
-      <StepperFormActions />
-    </div>
+        <StepperFormActions />
+      </form>
+    </Form>
   )
 }
 
