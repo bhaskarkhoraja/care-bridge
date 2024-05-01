ee9b22a9d334fe627fad03b8c80d989462baca61
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-05-01 19:28:18 +0545

    chore: check user details

--- apps/web/src/app/(marketing)/page.tsx
+++ apps/web/src/app/(marketing)/page.tsx
@@ -1,3 +1,11 @@
+import { getCurrentUser } from "@web/src/lib/session"
+
 export default async function Home() {
-  return <div></div>
+  const user = await getCurrentUser()
+
+  if (!user) {
+    return <h1>Not logged in</h1>
+  }
+
+  return <div className="w-full overflow-x-scroll">{JSON.stringify(user)}</div>
 }
