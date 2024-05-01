688b7e3e668a3411b16b32bc7e26e0ea1171def2
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-05-01 23:20:22 +0545

    feat: add profile_url for family member

--- apps/server/migrations/20240330120614_create_family_members_table.sql
+++ apps/server/migrations/20240330120614_create_family_members_table.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS family_member (
   middle_name VARCHAR(255),
   last_name VARCHAR(255) NOT NULL,
   gender user_gender,
+  profile_url TEXT NOT NULL,
   date_of_birth TIMESTAMPTZ NOT NULL,
   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
@@ -19,6 +20,7 @@ COMMENT ON COLUMN family_member.first_name IS 'First name of the family member';
 COMMENT ON COLUMN family_member.middle_name IS 'Middle name of the family member (if available)';
 COMMENT ON COLUMN family_member.last_name IS 'Last name of the family member';
 COMMENT ON COLUMN family_member.gender IS 'Gender (male, female, others)';
+COMMENT ON COLUMN family.profile_url IS 'URL pointing to the user profile image';
 COMMENT ON COLUMN family_member.date_of_birth IS 'Date of birth of the family member';
 COMMENT ON COLUMN family_member.updated_at IS 'Timestamp of the last update to the family member record';
 COMMENT ON COLUMN family_member.created_at IS 'Timestamp of when the family member record was created';
