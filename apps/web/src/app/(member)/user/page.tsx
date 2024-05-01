b9fbb6a60ac4b16ce1196f2b37fcbc457d4c9f61
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-05-01 19:28:47 +0545

    feat: user page

---
+++ apps/web/src/app/(member)/user/page.tsx
@@ -0,0 +1,7 @@
+export default async function User() {
+  return (
+    <main className="relative py-6 lg:gap-10 lg:py-8 xl:grid xl:grid-cols-[1fr_300px]">
+      <div className="mx-auto w-full min-w-0">User</div>
+    </main>
+  )
+}
