55bacdf547036e6e3316cc099348b84e48ce508a
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-05-01 22:52:40 +0545

    style: remove grid from layout
    grid was not needed

--- apps/web/src/app/(member)/user/layout.tsx
+++ apps/web/src/app/(member)/user/layout.tsx
@@ -3,9 +3,5 @@ interface UserLayoutProps {
 }
 
 export default function UserLayout({ children }: UserLayoutProps) {
-  return (
-    <div className="container flex-1 items-start sm:grid sm:grid-cols-[220px_minmax(0,1fr)] sm:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
-      {children}
-    </div>
-  )
+  return <div className="container mt-8 flex-1 items-start">{children}</div>
 }
