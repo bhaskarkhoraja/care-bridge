1dfaef20a82fd3df5ae80824b56a9b0c16877264
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-05-03 22:38:53 +0545

    feat: server action for family member

---
+++ apps/web/src/actions/user/family-member.ts
@@ -0,0 +1,115 @@
+"use server"
+
+import { cookies } from "next/headers"
+import { initClient } from "@ts-rest/core"
+import { webEnv } from "@web/src/lib/env"
+import { cookieName } from "@web/src/lib/utils"
+import {
+  FamilyInfoFormSchema,
+  FamilySpecialNeedsSchema,
+} from "node_modules/api-contract/dist/types/family-member.mjs"
+import { DocumentFormSchema } from "node_modules/api-contract/dist/types/user.mjs"
+import contract from "packages/api-contract/dist/index.mjs"
+import { z } from "zod"
+
+/**
+ * Get the family member info of the profile
+ **/
+export async function getFamilyMemberInfo(familyMemberId: string) {
+  const client = initClient(contract, {
+    baseUrl: webEnv.SERVER_URL,
+    baseHeaders: {
+      cookie: `${cookieName}=${cookies().get(cookieName)?.value}`,
+    },
+  })
+  return client.family.getFamilyMemberInfo({ params: { id: familyMemberId } })
+}
+
+/**
+ * Set the family member info of the profile
+ **/
+export async function setFamilyMemberInfo(
+  data: z.infer<typeof FamilyInfoFormSchema>,
+  familyMemberId: string
+) {
+  const client = initClient(contract, {
+    baseUrl: webEnv.SERVER_URL,
+    baseHeaders: {
+      cookie: `${cookieName}=${cookies().get(cookieName)?.value}`,
+    },
+  })
+  return client.family.setFamilyMemberInfo({
+    body: {
+      data: data,
+      familyMemberId: familyMemberId,
+    },
+  })
+}
+
+/**
+ * Get the family member special need
+ **/
+export async function getFamilySpecialNeed(familyMemberId: string) {
+  const client = initClient(contract, {
+    baseUrl: webEnv.SERVER_URL,
+    baseHeaders: {
+      cookie: `${cookieName}=${cookies().get(cookieName)?.value}`,
+    },
+  })
+  return client.family.getFamilySpecialNeed({ params: { id: familyMemberId } })
+}
+
+/**
+ * Set the family member special need
+ **/
+export async function setFamilySpecialNeed(
+  data: z.infer<typeof FamilySpecialNeedsSchema>,
+  familyMemberId: string
+) {
+  const client = initClient(contract, {
+    baseUrl: webEnv.SERVER_URL,
+    baseHeaders: {
+      cookie: `${cookieName}=${cookies().get(cookieName)?.value}`,
+    },
+  })
+  return client.family.setFamilySpecialNeed({
+    body: {
+      data: data,
+      familyMemberId: familyMemberId,
+    },
+  })
+}
+
+/**
+ * Get the family document info
+ **/
+export async function getFamilyDocumentInfo(familyMemberId: string) {
+  const client = initClient(contract, {
+    baseUrl: webEnv.SERVER_URL,
+    baseHeaders: {
+      cookie: `${cookieName}=${cookies().get(cookieName)?.value}`,
+    },
+  })
+  return client.family.getFamilyDocumentInfo({ params: { id: familyMemberId } })
+}
+
+/**
+ * Set the family document info
+ **/
+export async function setFamilyDocumentInfo(
+  data: z.infer<typeof DocumentFormSchema>,
+  familyMemberId: string
+) {
+  const client = initClient(contract, {
+    baseUrl: webEnv.SERVER_URL,
+    baseHeaders: {
+      cookie: `${cookieName}=${cookies().get(cookieName)?.value}`,
+    },
+  })
+  return client.family.setFamilyDocumentInfo({
+    body: {
+      data: data,
+      familyMemberId: familyMemberId,
+    },
+  })
+}
