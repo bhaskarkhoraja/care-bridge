bb76196cb4434b9ab232c367758b25a9753657f3
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-05-03 22:35:43 +0545

    feat: add title and refactor description_url

--- apps/server/migrations/20240330121009_create_special_needs_table.sql
+++ apps/server/migrations/20240330121009_create_special_needs_table.sql
@@ -2,8 +2,9 @@
 CREATE TABLE IF NOT EXISTS special_needs (
   id UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID(),
   family_member_id UUID REFERENCES family_member(id),
+  title TEXT NOT NULL,
   description TEXT NOT NULL,
-  description_url TEXT NOT NULL,
+  url TEXT NOT NULL,
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
@@ -12,7 +13,8 @@ CREATE TABLE IF NOT EXISTS special_needs (
 COMMENT ON TABLE special_needs IS 'Stores any special needs of members like physically able';
 COMMENT ON COLUMN special_needs.id IS 'Unique identifier for the special need';
 COMMENT ON COLUMN special_needs.family_member_id IS 'Foreign key referencing the family member associated with the special need';
+COMMENT ON COLUMN special_needs.title IS 'Title of the special need';
 COMMENT ON COLUMN special_needs.description IS 'Description of the special need';
-COMMENT ON COLUMN special_needs.description_url IS 'URL pointing to additional information about the special need';
+COMMENT ON COLUMN special_needs.url IS 'URL pointing to additional information about the special need';
 COMMENT ON COLUMN special_needs.created_at IS 'Timestamp of when the special need record was created';
 COMMENT ON COLUMN special_needs.updated_at IS 'Timestamp of the last update to the special need record';
