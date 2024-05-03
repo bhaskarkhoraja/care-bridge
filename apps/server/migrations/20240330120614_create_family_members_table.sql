911243b0c538f0c2aa0bc8c77d1f5cf149acce3a
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-05-03 22:36:06 +0545

    fix: profile_url should be nullable

--- apps/server/migrations/20240330120614_create_family_members_table.sql
+++ apps/server/migrations/20240330120614_create_family_members_table.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS family_member (
   middle_name VARCHAR(255),
   last_name VARCHAR(255) NOT NULL,
   gender user_gender,
-  profile_url TEXT NOT NULL,
+  profile_url TEXT,
   date_of_birth TIMESTAMPTZ NOT NULL,
   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
