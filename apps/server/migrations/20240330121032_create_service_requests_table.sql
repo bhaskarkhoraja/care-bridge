6c5008511b63acbe7f7c03b994ea6df43fdc3234
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-05-07 08:44:46 +0545

    feat: add draft to status and add location

--- apps/server/migrations/20240330121032_create_service_requests_table.sql
+++ apps/server/migrations/20240330121032_create_service_requests_table.sql
@@ -1,5 +1,5 @@
 -- table to store service requests
-CREATE TYPE service_status AS ENUM ('open', 'close');
+CREATE TYPE service_status AS ENUM ('open', 'close', 'draft');
 
 CREATE TABLE IF NOT EXISTS service_request (
   id UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID(),
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS service_request (
   description TEXT NOT NULL,
   start_time TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   end_time TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  status service_status,
+  status service_status DEFAULT 'draft',
   prefered_language VARCHAR(255) DEFAULT NULL,
   mandatory_language BOOLEAN DEFAULT FALSE,
   prefered_gender user_gender,
@@ -16,6 +16,7 @@ CREATE TABLE IF NOT EXISTS service_request (
   mandatory_age BOOLEAN DEFAULT FALSE,
   prefered_citizen VARCHAR(255) DEFAULT NULL,
   mandatory_citizen BOOLEAN DEFAULT FALSE,
+  location TEXT NOT NULL,
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
@@ -37,5 +38,6 @@ COMMENT ON COLUMN service_request.prefered_age IS 'Preferred age range for the s
 COMMENT ON COLUMN service_request.mandatory_age IS 'Indicates if age preference is mandatory';
 COMMENT ON COLUMN service_request.prefered_citizen IS 'Preferred citizenship for the service provider';
 COMMENT ON COLUMN service_request.mandatory_citizen IS 'Indicates if citizenship preference is mandatoray';
+COMMENT ON COLUMN service_request.location IS 'Location of the service request';
 COMMENT ON COLUMN service_request.created_at IS 'Timestamp of when the service request was created';
 COMMENT ON COLUMN service_request.updated_at IS 'Timestamp of the last update to the service request';
