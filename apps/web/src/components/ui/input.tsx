68adcd34f228644f91e2b554b7ce995502b2000f
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-05-10 23:00:14 +0545

    style: decrease size of input if hidden
    don't actually hide just decrease input size as we need to scroll after validation

--- apps/web/src/components/ui/input.tsx
+++ apps/web/src/components/ui/input.tsx
@@ -5,17 +5,21 @@ import { cn } from "@web/src/lib/utils"
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string
+  hidden?: boolean
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, label, ...props }, ref) => {
+  ({ className, type, label, hidden, ...props }, ref) => {
     return (
-      <div className="relative w-full">
+      <div className={cn("relative w-full", hidden ? "size-0" : "")}>
         <input
           type={type}
           className={cn(
             "border-input bg-background placeholder:text-muted-foreground focus-visible:ring-ring ring-offset-background peer flex h-10 w-full rounded-md border p-2 pt-5 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-            className
+            className,
+            hidden
+              ? "size-0 border-0 bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0"
+              : ""
           )}
           placeholder=""
           ref={ref}
@@ -23,7 +27,10 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         />
         <Label
           htmlFor={props.id}
-          className="text-muted-foreground peer-focus:text-muted-foreground peer-focus-within:text-muted-foreground peer-focus-visible:text-muted-foreground peer-placeholder-shown:text-muted-foreground absolute start-2 top-3.5 z-10 origin-[0] translate-y-[-13px] scale-75 text-sm duration-100 peer-placeholder-shown:-translate-y-1 peer-placeholder-shown:scale-100 peer-focus:translate-y-[-13px] peer-focus:scale-75 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4"
+          className={cn(
+            "text-muted-foreground peer-focus:text-muted-foreground peer-focus-within:text-muted-foreground peer-focus-visible:text-muted-foreground peer-placeholder-shown:text-muted-foreground absolute start-2 top-3.5 z-10 origin-[0] translate-y-[-13px] scale-75 text-sm duration-100 peer-placeholder-shown:-translate-y-1 peer-placeholder-shown:scale-100 peer-focus:translate-y-[-13px] peer-focus:scale-75 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4",
+            hidden ? "hidden" : ""
+          )}
         >
           {label}
         </Label>
