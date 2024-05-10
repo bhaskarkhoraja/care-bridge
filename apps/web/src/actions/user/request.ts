ac539816a6af79dd2c4a73ef9de69d4bf9f439d2
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-05-10 23:02:01 +0545

    feat: actions for request

---
+++ apps/web/src/actions/user/request.ts
@@ -0,0 +1,48 @@
+"use server"
+
+import { cookies } from "next/headers"
+import { initClient } from "@ts-rest/core"
+import { webEnv } from "@web/src/lib/env"
+import { cookieName } from "@web/src/lib/utils"
+import contract from "api-contract"
+import { RequestSchema } from "api-contract/types"
+import { z } from "zod"
+
+/**
+ * Add Request
+ **/
+export async function createRequest(request: z.infer<typeof RequestSchema>) {
+  const client = initClient(contract, {
+    baseUrl: webEnv.SERVER_URL,
+    baseHeaders: {
+      cookie: `${cookieName}=${cookies().get(cookieName)?.value}`,
+    },
+  })
+  return client.request.createRequest({ body: request })
+}
+
+/**
+ * Get all seller request
+ **/
+export async function getMyRequest() {
+  const client = initClient(contract, {
+    baseUrl: webEnv.SERVER_URL,
+    baseHeaders: {
+      cookie: `${cookieName}=${cookies().get(cookieName)?.value}`,
+    },
+  })
+  return client.request.getMyRequest()
+}
+
+/**
+ * Get specific request
+ **/
+export async function getRequest(requestId: string) {
+  const client = initClient(contract, {
+    baseUrl: webEnv.SERVER_URL,
+    baseHeaders: {
+      cookie: `${cookieName}=${cookies().get(cookieName)?.value}`,
+    },
+  })
+  return client.request.getRequest({ params: { id: requestId } })
+}
