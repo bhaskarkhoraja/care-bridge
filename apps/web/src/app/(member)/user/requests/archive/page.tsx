1fdf5d2a198a7c375ab45105b283d92d0bbd8e27
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-05-11 12:22:33 +0545

    perf: check for empty request

--- apps/web/src/app/(member)/user/requests/archive/page.tsx
+++ apps/web/src/app/(member)/user/requests/archive/page.tsx
@@ -1,11 +1,23 @@
 import { getMyRequest } from "@web/src/actions/user/request"
+import { RequestSchema } from "api-contract/types"
+import { z } from "zod"
 
 import RequestCard from "../_components/request-card"
 
 export default async function DraftRequest() {
   const myRequest = await getMyRequest()
 
-  if (myRequest.status !== 200) {
+  let filteredRequest: z.infer<typeof RequestSchema>[]
+
+  if (myRequest.status === 200) {
+    filteredRequest = myRequest.body.data.filter(
+      (request) => request.status === "close"
+    )
+  } else {
+    filteredRequest = []
+  }
+
+  if (myRequest.status !== 200 || filteredRequest.length === 0) {
     return (
       <main className="w-full">
         <h1 className="mb-4 text-2xl font-bold leading-none">
@@ -13,7 +25,9 @@ export default async function DraftRequest() {
         </h1>
 
         <p className="text-muted-foreground text-sm">
-          {myRequest.body.message || "No requests found"}
+          {myRequest.status !== 200
+            ? myRequest.body.message || "No requests found"
+            : "No requests found"}
         </p>
       </main>
     )
@@ -22,15 +36,13 @@ export default async function DraftRequest() {
     <main className="w-full">
       <h1 className="mb-4 text-2xl font-bold leading-none">Archive requests</h1>
       <div className="gird-cols-1 grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
-        {myRequest.body.data
-          .filter((request) => request.status === "close")
-          .map((request, requestIndex) => (
-            <RequestCard
-              key={request.id}
-              request={request}
-              index={requestIndex}
-            />
-          ))}
+        {filteredRequest.map((request, requestIndex) => (
+          <RequestCard
+            key={request.id}
+            request={request}
+            index={requestIndex}
+          />
+        ))}
       </div>
     </main>
   )
