d932ab282207a34b950edebfb327880796690410
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-05-16 23:53:23 +0545

    refactor: keep function inside useeffect

--- apps/web/src/app/(member)/user/message/_components/side-message.tsx
+++ apps/web/src/app/(member)/user/message/_components/side-message.tsx
@@ -44,112 +44,6 @@ const SideMessage: React.FC<SideMessageProps> = ({ data, user }) => {
     setMounted(true)
   }, [])
 
-  const updateMessage = async (newMessage: z.infer<typeof MessageSchema>) => {
-    if (
-      newMessage.senderProfileId !== user.profile_id &&
-      newMessage.recieverProfileId !== user.profile_id
-    )
-      return
-    const existingMessageIndex = messages.findIndex((msg) => {
-      if (newMessage.senderProfileId === user.profile_id) {
-        return (
-          msg.recieverProfile.id === newMessage.recieverProfileId ||
-          msg.senderProfile.id === newMessage.recieverProfileId
-        )
-      } else if (newMessage.recieverProfileId === user.profile_id) {
-        return (
-          msg.recieverProfile.id === newMessage.senderProfileId ||
-          msg.senderProfile.id === newMessage.senderProfileId
-        )
-      } else {
-        return false
-      }
-    })
-
-    if (existingMessageIndex !== -1) {
-      const updatedMessages = [...messages]
-      const existingMessage = updatedMessages.splice(existingMessageIndex, 1)[0]
-
-      existingMessage.message = newMessage.message
-      existingMessage.createdAt = newMessage.createdAt
-      existingMessage.isRead = newMessage.isRead
-      existingMessage.id = newMessage.id
-
-      updatedMessages.unshift(existingMessage)
-      setMessages(updatedMessages)
-    } else {
-      const newUserProfile = await getSenderDetails(
-        newMessage.senderProfileId === user.profile_id
-          ? newMessage.recieverProfileId
-          : newMessage.senderProfileId
-      )
-      if (newUserProfile.status !== 200) return
-      const updatedMessages = [...messages]
-      const newUserMessage: z.infer<typeof SideMessageSchema> = {
-        id: newMessage.id,
-        message: newMessage.message,
-        isRead: newMessage.isRead,
-        createdAt: newMessage.createdAt,
-        senderProfile:
-          newMessage.senderProfileId === user.profile_id
-            ? messages[0].senderProfile.id === user.profile_id
-              ? messages[0].senderProfile
-              : messages[0].recieverProfile
-            : {
-                id: newMessage.senderProfileId,
-                firstName: newUserProfile.body.data.firstName,
-                middleName: newUserProfile.body.data.middleName,
-                lastName: newUserProfile.body.data.lastName,
-                gender: "others", // kept temporarly to fullfill types
-                email: newUserProfile.body.data.email,
-                dob: new Date(), // kept temporarly to fullfill types
-                userName: "newUser", // kept temporarly to fullfill types
-                profileUrl: newUserProfile.body.data.profileUrl,
-              },
-        recieverProfile:
-          newMessage.recieverProfileId === user.profile_id
-            ? messages[0].recieverProfile.id === user.profile_id
-              ? messages[0].recieverProfile
-              : messages[0].senderProfile
-            : {
-                id: newMessage.senderProfileId,
-                firstName: newUserProfile.body.data.firstName,
-                middleName: newUserProfile.body.data.middleName,
-                lastName: newUserProfile.body.data.lastName,
-                gender: "others", // kept temporarly to fullfill types
-                email: newUserProfile.body.data.email,
-                dob: new Date(), // kept temporarly to fullfill types
-                userName: "newUser", // kept temporarly to fullfill types
-                profileUrl: newUserProfile.body.data.profileUrl,
-              },
-      }
-
-      updatedMessages.unshift(newUserMessage)
-      setMessages(updatedMessages)
-    }
-  }
-
-  const seenMessage = (newMessage: z.infer<typeof MessageSchema>) => {
-    const matchingMessage = messages.find(
-      (message) =>
-        (message.senderProfile.id === newMessage.senderProfileId &&
-          message.recieverProfile.id === newMessage.recieverProfileId) ||
-        (message.senderProfile.id === newMessage.recieverProfileId &&
-          message.recieverProfile.id === newMessage.senderProfileId)
-    )
-
-    if (!matchingMessage) {
-      return
-    }
-
-    if (!matchingMessage.isRead) {
-      const updatedMessages = messages.map((message) =>
-        message === matchingMessage ? { ...message, isRead: true } : message
-      )
-      setMessages(updatedMessages)
-    }
-  }
-
   useEffect(() => {
     socket.on("recieveMessage", (newMessage: z.infer<typeof MessageSchema>) => {
       updateMessage(newMessage)
@@ -157,7 +51,115 @@ const SideMessage: React.FC<SideMessageProps> = ({ data, user }) => {
     socket.on("seenMessage", (newMessage: z.infer<typeof MessageSchema>) => {
       seenMessage(newMessage)
     })
-  }, [messages, socket, user.profile_id, seenMessage, updateMessage])
+    const updateMessage = async (newMessage: z.infer<typeof MessageSchema>) => {
+      if (
+        newMessage.senderProfileId !== user.profile_id &&
+        newMessage.recieverProfileId !== user.profile_id
+      )
+        return
+      const existingMessageIndex = messages.findIndex((msg) => {
+        if (newMessage.senderProfileId === user.profile_id) {
+          return (
+            msg.recieverProfile.id === newMessage.recieverProfileId ||
+            msg.senderProfile.id === newMessage.recieverProfileId
+          )
+        } else if (newMessage.recieverProfileId === user.profile_id) {
+          return (
+            msg.recieverProfile.id === newMessage.senderProfileId ||
+            msg.senderProfile.id === newMessage.senderProfileId
+          )
+        } else {
+          return false
+        }
+      })
+
+      if (existingMessageIndex !== -1) {
+        const updatedMessages = [...messages]
+        const existingMessage = updatedMessages.splice(
+          existingMessageIndex,
+          1
+        )[0]
+
+        existingMessage.message = newMessage.message
+        existingMessage.createdAt = newMessage.createdAt
+        existingMessage.isRead = newMessage.isRead
+        existingMessage.id = newMessage.id
+
+        updatedMessages.unshift(existingMessage)
+        setMessages(updatedMessages)
+      } else {
+        const newUserProfile = await getSenderDetails(
+          newMessage.senderProfileId === user.profile_id
+            ? newMessage.recieverProfileId
+            : newMessage.senderProfileId
+        )
+        if (newUserProfile.status !== 200) return
+        const updatedMessages = [...messages]
+        const newUserMessage: z.infer<typeof SideMessageSchema> = {
+          id: newMessage.id,
+          message: newMessage.message,
+          isRead: newMessage.isRead,
+          createdAt: newMessage.createdAt,
+          senderProfile:
+            newMessage.senderProfileId === user.profile_id
+              ? messages[0].senderProfile.id === user.profile_id
+                ? messages[0].senderProfile
+                : messages[0].recieverProfile
+              : {
+                  id: newMessage.senderProfileId,
+                  firstName: newUserProfile.body.data.firstName,
+                  middleName: newUserProfile.body.data.middleName,
+                  lastName: newUserProfile.body.data.lastName,
+                  gender: "others", // kept temporarly to fullfill types
+                  email: newUserProfile.body.data.email,
+                  dob: new Date(), // kept temporarly to fullfill types
+                  userName: "newUser", // kept temporarly to fullfill types
+                  profileUrl: newUserProfile.body.data.profileUrl,
+                },
+          recieverProfile:
+            newMessage.recieverProfileId === user.profile_id
+              ? messages[0].recieverProfile.id === user.profile_id
+                ? messages[0].recieverProfile
+                : messages[0].senderProfile
+              : {
+                  id: newMessage.senderProfileId,
+                  firstName: newUserProfile.body.data.firstName,
+                  middleName: newUserProfile.body.data.middleName,
+                  lastName: newUserProfile.body.data.lastName,
+                  gender: "others", // kept temporarly to fullfill types
+                  email: newUserProfile.body.data.email,
+                  dob: new Date(), // kept temporarly to fullfill types
+                  userName: "newUser", // kept temporarly to fullfill types
+                  profileUrl: newUserProfile.body.data.profileUrl,
+                },
+        }
+
+        updatedMessages.unshift(newUserMessage)
+        setMessages(updatedMessages)
+      }
+    }
+
+    const seenMessage = (newMessage: z.infer<typeof MessageSchema>) => {
+      const matchingMessage = messages.find(
+        (message) =>
+          (message.senderProfile.id === newMessage.senderProfileId &&
+            message.recieverProfile.id === newMessage.recieverProfileId) ||
+          (message.senderProfile.id === newMessage.recieverProfileId &&
+            message.recieverProfile.id === newMessage.senderProfileId)
+      )
+
+      if (!matchingMessage) {
+        return
+      }
+
+      if (!matchingMessage.isRead) {
+        const updatedMessages = messages.map((message) =>
+          message === matchingMessage ? { ...message, isRead: true } : message
+        )
+        setMessages(updatedMessages)
+      }
+    }
+  }, [messages, socket, user.profile_id])
 
   return (
     <div>
