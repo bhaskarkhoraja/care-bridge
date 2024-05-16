adb98076e6f0ae3758438c9d559a726734aceeca
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-05-16 23:56:25 +0545

    style: remove space-y in big screen

--- apps/web/src/app/(member)/user/archive-message/layout.tsx
+++ apps/web/src/app/(member)/user/archive-message/layout.tsx
@@ -21,7 +21,7 @@ export default async function ArchiveMessageLayout({
 
   return (
     <main className="w-full">
-      <div className="flex-1 items-start space-y-5 lg:grid lg:grid-cols-[360px_minmax(0,1fr)]">
+      <div className="flex-1 items-start space-y-5 lg:grid lg:grid-cols-[360px_minmax(0,1fr)] lg:space-y-0">
         <div className="mt-4 flex h-[209px] w-full flex-col gap-2 border-b md:sticky lg:z-30 lg:-ml-2 lg:mt-0 lg:h-[calc(100vh-6rem)] lg:shrink-0 lg:border-b-0 lg:border-r lg:pr-4">
           <h1 className="mb-4 text-2xl font-bold leading-none">
             Archive Messages
