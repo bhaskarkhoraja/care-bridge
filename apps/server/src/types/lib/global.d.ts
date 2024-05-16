b6921b279c283224c3036301fe91b28bbb7d0449
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-05-16 09:10:30 +0545

    feat: add username in socket

--- apps/server/src/types/lib/global.d.ts
+++ apps/server/src/types/lib/global.d.ts
@@ -1,5 +1,6 @@
 import { AdapterUser } from 'next-auth/adapters'
 import '@app-type/lib/next-auth'
+import '@app-type/lib/socket-io'
 
 declare module 'express' {
   interface Request {
---
+++ types/lib/socket-io.ts
@@ -0,0 +1,5 @@
+declare module "socket.io" {
+  interface Socket {
+    username: string
+  }
+}
