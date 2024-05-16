894a5fd85f8c836d51ec2158b641bb097b196064
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-05-16 20:14:59 +0545

    feat: format time distance with fns

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { clsx, type ClassValue } from "clsx"
+import { FormatDistanceFnOptions, FormatDistanceToken } from "date-fns"
 import { twMerge } from "tailwind-merge"
 
 import { webEnv } from "./env"
@@ -69,4 +70,52 @@ export function getShortName(name: string): string {
   }
 
   return shortName
+}
+
+/**
+ * Format distance option for time
+ **/
+const formatDistanceLocale = {
+  lessThanXSeconds: "{{count}}s",
+  xSeconds: "{{count}}s",
+  halfAMinute: "30s",
+  lessThanXMinutes: "{{count}}m",
+  xMinutes: "{{count}}m",
+  aboutXHours: "{{count}}h",
+  xHours: "{{count}}h",
+  xDays: "{{count}}d",
+  aboutXWeeks: "{{count}}w",
+  xWeeks: "{{count}}w",
+  aboutXMonths: "{{count}}m",
+  xMonths: "{{count}}m",
+  aboutXYears: "{{count}}y",
+  xYears: "{{count}}y",
+  overXYears: "{{count}}y",
+  almostXYears: "{{count}}y",
+}
+
+/**
+ * Format distance function
+ **/
+export function formatDistance(
+  token: FormatDistanceToken,
+  count: number,
+  options?: FormatDistanceFnOptions
+) {
+  options = options || {}
+
+  const result = formatDistanceLocale[token].replace(
+    "{{count}}",
+    count.toString()
+  )
+
+  if (options.addSuffix) {
+    if (options && options.comparison && options.comparison > 0) {
+      return "in " + result
+    } else {
+      return result + " ago"
+    }
+  }
+
+  return result
 }
