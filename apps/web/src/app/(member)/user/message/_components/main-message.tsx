e4eb2883e02c00f3c20a6aadf53e1739aa891f30
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-05-16 09:32:04 +0545

    feat: open message page

---
+++ apps/web/src/app/(member)/user/message/_components/main-message.tsx
@@ -0,0 +1,234 @@
+"use client"
+
+import { FormEvent, useEffect, useMemo, useRef, useState } from "react"
+import { useRouter } from "next/navigation"
+import { ScrollArea } from "@radix-ui/react-scroll-area"
+import { markMessages } from "@web/src/actions/user/message"
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@web/src/components/ui/avatar"
+import { Button } from "@web/src/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@web/src/components/ui/dropdown-menu"
+import { Input } from "@web/src/components/ui/input"
+import { webEnv } from "@web/src/lib/env"
+import { cn, getShortName } from "@web/src/lib/utils"
+import { MessageSchema, SenderDetailsSchema } from "api-contract/types"
+import { Ellipsis, SendHorizonal } from "lucide-react"
+import { Session } from "next-auth"
+import NProgress from "nprogress"
+import { io } from "socket.io-client"
+import { toast } from "sonner"
+import { v4 as uuidv4 } from "uuid"
+import { z } from "zod"
+
+interface MainMessageProps {
+  data: z.infer<typeof MessageSchema>[] | undefined
+  senderId: string
+  senderDetails: z.infer<typeof SenderDetailsSchema>
+  user: Session["user"]
+}
+
+const MainMessage: React.FC<MainMessageProps> = ({
+  data,
+  senderId,
+  senderDetails,
+  user,
+}) => {
+  const [messages, setMessages] = useState<
+    z.infer<typeof MessageSchema>[] | undefined
+  >(data)
+  const messageEndRef = useRef<HTMLDivElement | null>(null)
+  const [input, setInput] = useState("")
+  const socket = useMemo(
+    () =>
+      io(`${webEnv.NEXT_PUBLIC_SERVER_URL}`, {
+        autoConnect: true,
+        path: "/user/message/socket.io",
+        auth: { username: user.id },
+      }),
+    []
+  )
+  const router = useRouter()
+
+  useEffect(() => {
+    socket.on("recieveMessage", (newMessage: z.infer<typeof MessageSchema>) => {
+      if (
+        newMessage &&
+        newMessage.senderProfileId === senderId &&
+        newMessage.recieverProfileId === user.profile_id
+      ) {
+        setMessages((prevMessages) => {
+          if (
+            prevMessages &&
+            !prevMessages.some((msg) => msg.id === newMessage.id)
+          ) {
+            return [...prevMessages, newMessage]
+          } else {
+            return prevMessages || [{ ...newMessage }]
+          }
+        })
+      }
+    })
+    setTimeout(() => {
+      if (messageEndRef && messageEndRef.current) {
+        messageEndRef.current.scrollIntoView({ block: "end" })
+      }
+    }, 100)
+  }, [])
+
+  const handleSendMessage = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    if (!input.length) return
+
+    const newMessage: z.infer<typeof MessageSchema> = {
+      id: uuidv4(),
+      message: input,
+      createdAt: new Date(),
+      isRead: false,
+      senderProfileId: user.profile_id as string,
+      recieverProfileId: senderId,
+    }
+
+    socket.emit("sendMessage", newMessage)
+
+    setMessages((prevMessages) => {
+      if (prevMessages) {
+        return [...prevMessages, newMessage]
+      } else {
+        return [newMessage]
+      }
+    })
+    setInput("")
+    setTimeout(() => {
+      if (messageEndRef && messageEndRef.current) {
+        messageEndRef.current.scrollIntoView({ block: "end" })
+      }
+    }, 100)
+  }
+
+  const updateMessageStaus = (action: "close" | "spam") => {
+    toast.promise(pendingActions(action), {
+      loading: "Loading...",
+      success: (msg) => {
+        NProgress.start()
+        router.push(
+          `/user/${action === "spam" ? "spam-message" : "archive-message"}/${senderId}`
+        )
+        return msg
+      },
+      error: (msg) => msg,
+    })
+  }
+
+  const pendingActions = async (action: "close" | "spam") => {
+    const result = await markMessages(senderId, action)
+
+    if (result.status === 200) {
+      return result.body.message
+    } else {
+      throw new Error(result.body.message)
+    }
+  }
+
+  return (
+    <main>
+      <div className="mb-4 flex w-full items-center justify-between">
+        <div className="flex items-center gap-4">
+          <Avatar className="size-10">
+            <AvatarImage
+              src={senderDetails.profileUrl}
+              alt={
+                senderDetails.middleName
+                  ? `${senderDetails.firstName} ${senderDetails.middleName} ${senderDetails.lastName}`
+                  : `${senderDetails.firstName} ${senderDetails.lastName}`
+              }
+            />
+            <AvatarFallback className="text-md">
+              {getShortName(
+                senderDetails.middleName
+                  ? `${senderDetails.firstName} ${senderDetails.middleName} ${senderDetails.lastName}`
+                  : `${senderDetails.firstName} ${senderDetails.lastName}`
+              )}
+            </AvatarFallback>
+          </Avatar>
+          <div>
+            <p className="font-semibold">
+              {senderDetails.middleName
+                ? `${senderDetails.firstName} ${senderDetails.middleName} ${senderDetails.lastName}`
+                : `${senderDetails.firstName} ${senderDetails.lastName}`}
+            </p>
+            <p className="text-muted-foreground text-sm">
+              {senderDetails.email}
+            </p>
+          </div>
+        </div>
+        <div>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button type="button" size="icon">
+                <Ellipsis className="size-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem onClick={() => updateMessageStaus("close")}>
+                Archive chat
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => updateMessageStaus("spam")}>
+                Mark as spam
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
+      <div className="w-full border-t pt-4 md:sticky lg:z-30 lg:-ml-2 lg:mt-0 lg:h-[calc(100vh-14rem)] lg:shrink-0">
+        <ScrollArea className="h-full">
+          {messages ? (
+            messages.map((message, index) => (
+              <div
+                key={index}
+                className={cn(
+                  "mb-1 flex w-fit max-w-[75%] flex-col gap-2 text-wrap rounded-lg px-3 py-2 text-sm",
+                  message.senderProfileId === senderId
+                    ? "bg-muted"
+                    : "bg-foreground text-background/95 ml-auto mr-4"
+                )}
+              >
+                <p>{message.message}</p>
+              </div>
+            ))
+          ) : (
+            <p></p>
+          )}
+          <div ref={messageEndRef} />
+        </ScrollArea>
+      </div>
+      <form
+        onSubmit={handleSendMessage}
+        className="mt-4 flex w-full items-center space-x-2"
+      >
+        <Input
+          label=""
+          id="message"
+          placeholder="Type your message..."
+          className="flex-1 p-2"
+          autoComplete="off"
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <Button type="submit" size="icon" disabled={input.length === 0}>
+          <SendHorizonal className="size-5" />
+          <span className="sr-only">Send</span>
+        </Button>
+      </form>
+    </main>
+  )
+}
+
+export default MainMessage
---
+++ apps/web/src/app/(member)/user/message/_components/side-message.tsx
@@ -0,0 +1,234 @@
+"use client"
+
+import { useEffect, useMemo, useState } from "react"
+import Link from "next/link"
+import { useSelectedLayoutSegment } from "next/navigation"
+import { getSenderDetails } from "@web/src/actions/user/message"
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@web/src/components/ui/avatar"
+import { webEnv } from "@web/src/lib/env"
+import { cn, getShortName } from "@web/src/lib/utils"
+import { MessageSchema, SideMessageSchema } from "api-contract/types"
+import { formatDistanceToNow } from "date-fns"
+import { Dot } from "lucide-react"
+import { Session } from "next-auth"
+import { io } from "socket.io-client"
+import { z } from "zod"
+
+interface SideMessageProps {
+  data: z.infer<typeof SideMessageSchema>[]
+  user: Session["user"]
+}
+
+const SideMessage: React.FC<SideMessageProps> = ({ data, user }) => {
+  const segment = useSelectedLayoutSegment()
+  const [messages, setMessages] =
+    useState<z.infer<typeof SideMessageSchema>[]>(data)
+  const socket = useMemo(
+    () =>
+      io(`${webEnv.NEXT_PUBLIC_SERVER_URL}`, {
+        autoConnect: true,
+        path: "/user/message/socket.io",
+        auth: { username: user.id },
+      }),
+    []
+  )
+
+  useEffect(() => {
+    socket.on("recieveMessage", (newMessage: z.infer<typeof MessageSchema>) => {
+      updateMessage(newMessage)
+    })
+
+    const updateMessage = async (newMessage: z.infer<typeof MessageSchema>) => {
+      if (
+        newMessage.senderProfileId !== user.profile_id &&
+        newMessage.recieverProfileId !== user.profile_id
+      )
+        return
+      const existingMessageIndex = messages.findIndex((msg) => {
+        if (newMessage.senderProfileId === user.profile_id) {
+          return (
+            msg.recieverProfile.id === newMessage.recieverProfileId ||
+            msg.senderProfile.id === newMessage.recieverProfileId
+          )
+        } else if (newMessage.recieverProfileId === user.profile_id) {
+          return (
+            msg.recieverProfile.id === newMessage.senderProfileId ||
+            msg.senderProfile.id === newMessage.senderProfileId
+          )
+        } else {
+          return false
+        }
+      })
+
+      if (existingMessageIndex !== -1) {
+        const updatedMessages = [...messages]
+        const existingMessage = updatedMessages.splice(
+          existingMessageIndex,
+          1
+        )[0]
+
+        existingMessage.message = newMessage.message
+        existingMessage.createdAt = newMessage.createdAt
+        existingMessage.isRead = newMessage.isRead
+        existingMessage.id = newMessage.id
+
+        updatedMessages.unshift(existingMessage)
+        setMessages(updatedMessages)
+      } else {
+        const newUserProfile = await getSenderDetails(
+          newMessage.senderProfileId === user.profile_id
+            ? newMessage.recieverProfileId
+            : newMessage.senderProfileId
+        )
+        if (newUserProfile.status !== 200) return
+        const updatedMessages = [...messages]
+        const newUserMessage: z.infer<typeof SideMessageSchema> = {
+          id: newMessage.id,
+          message: newMessage.message,
+          isRead: newMessage.isRead,
+          createdAt: newMessage.createdAt,
+          senderProfile:
+            newMessage.senderProfileId === user.profile_id
+              ? messages[0].senderProfile.id === user.profile_id
+                ? messages[0].senderProfile
+                : messages[0].recieverProfile
+              : {
+                  id: newMessage.senderProfileId,
+                  firstName: newUserProfile.body.data.firstName,
+                  middleName: newUserProfile.body.data.middleName,
+                  lastName: newUserProfile.body.data.lastName,
+                  gender: "others", // kept temporarly to fullfill types
+                  email: newUserProfile.body.data.email,
+                  dob: new Date(), // kept temporarly to fullfill types
+                  userName: "newUser", // kept temporarly to fullfill types
+                  profileUrl: newUserProfile.body.data.profileUrl,
+                },
+          recieverProfile:
+            newMessage.recieverProfileId === user.profile_id
+              ? messages[0].recieverProfile.id === user.profile_id
+                ? messages[0].recieverProfile
+                : messages[0].senderProfile
+              : {
+                  id: newMessage.senderProfileId,
+                  firstName: newUserProfile.body.data.firstName,
+                  middleName: newUserProfile.body.data.middleName,
+                  lastName: newUserProfile.body.data.lastName,
+                  gender: "others", // kept temporarly to fullfill types
+                  email: newUserProfile.body.data.email,
+                  dob: new Date(), // kept temporarly to fullfill types
+                  userName: "newUser", // kept temporarly to fullfill types
+                  profileUrl: newUserProfile.body.data.profileUrl,
+                },
+        }
+
+        updatedMessages.unshift(newUserMessage)
+        setMessages(updatedMessages)
+      }
+    }
+  }, [])
+
+  return (
+    <div>
+      {messages.map((message) => (
+        <Link
+          href={`/user/message/${message.senderProfile.id === user.profile_id ? message.recieverProfile.id : message.senderProfile.id}`}
+          key={message.id}
+          className={cn(
+            "hover:bg-foreground/5 relative flex cursor-pointer gap-4 rounded-md p-4",
+            segment ===
+              (message.senderProfile.id === user.profile_id
+                ? message.recieverProfile.id
+                : message.senderProfile.id) &&
+              "bg-foreground/90 text-background hover:bg-foreground/90"
+          )}
+        >
+          {!message.isRead &&
+            user.profile_id !== message.senderProfile.id &&
+            segment !==
+              (message.senderProfile.id === user.profile_id
+                ? message.recieverProfile.id
+                : message.senderProfile.id) && (
+              <div className="absolute right-3 top-0 flex h-full items-center justify-center">
+                <div className="bg-foreground size-3 rounded-full" />
+              </div>
+            )}
+          <Avatar className="size-10">
+            <AvatarImage
+              src={
+                message.senderProfile.id === user.profile_id
+                  ? message.recieverProfile.profileUrl
+                  : message.senderProfile.profileUrl
+              }
+              alt={
+                message.senderProfile.id === user.profile_id
+                  ? message.recieverProfile.middleName
+                    ? `${message.recieverProfile.firstName} ${message.recieverProfile.middleName} ${message.recieverProfile.lastName}`
+                    : `${message.recieverProfile.firstName} ${message.recieverProfile.lastName}`
+                  : message.senderProfile.middleName
+                    ? `${message.senderProfile.firstName} ${message.senderProfile.middleName} ${message.senderProfile.lastName}`
+                    : `${message.senderProfile.firstName} ${message.senderProfile.lastName}`
+              }
+            />
+            <AvatarFallback
+              className={cn(
+                "text-md",
+                segment === user.profile_id
+                  ? message.recieverProfile.profileUrl
+                  : message.senderProfile.profileUrl &&
+                      "bg-background text-foreground"
+              )}
+            >
+              {getShortName(
+                message.senderProfile.id === user.profile_id
+                  ? message.recieverProfile.middleName
+                    ? `${message.recieverProfile.firstName} ${message.recieverProfile.middleName} ${message.recieverProfile.lastName}`
+                    : `${message.recieverProfile.firstName} ${message.recieverProfile.lastName}`
+                  : message.senderProfile.middleName
+                    ? `${message.senderProfile.firstName} ${message.senderProfile.middleName} ${message.senderProfile.lastName}`
+                    : `${message.senderProfile.firstName} ${message.senderProfile.lastName}`
+              )}
+            </AvatarFallback>
+          </Avatar>
+          <div>
+            <p className={cn("font-semibold", !message.isRead && "font-bold")}>
+              {message.senderProfile.id === user.profile_id
+                ? message.recieverProfile.middleName
+                  ? `${message.recieverProfile.firstName} ${message.recieverProfile.middleName} ${message.recieverProfile.lastName}`
+                  : `${message.recieverProfile.firstName} ${message.recieverProfile.lastName}`
+                : message.senderProfile.middleName
+                  ? `${message.senderProfile.firstName} ${message.senderProfile.middleName} ${message.senderProfile.lastName}`
+                  : `${message.senderProfile.firstName} ${message.senderProfile.lastName}`}
+            </p>
+            <p
+              className={cn(
+                "text-muted-foreground flex items-center text-sm",
+                segment ===
+                  (message.senderProfile.id === user.profile_id
+                    ? message.recieverProfile.id
+                    : message.senderProfile.id) && "text-background/80",
+                !message.isRead &&
+                  user.profile_id !== message.senderProfile.id &&
+                  segment !==
+                    (message.senderProfile.id === user.profile_id
+                      ? message.recieverProfile.id
+                      : message.senderProfile.id) &&
+                  "text-foreground font-semibold"
+              )}
+            >
+              {message.message} <Dot />{" "}
+              {formatDistanceToNow(new Date(message.createdAt), {
+                addSuffix: false,
+              }).replace(/^about\s+/, "")}
+            </p>
+          </div>
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+export default SideMessage
