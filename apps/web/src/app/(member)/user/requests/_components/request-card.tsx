3e11841f22918f2ad38f0a1ded1b5bb0f8d42464
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-05-18 18:28:02 +0545

    feat: show paid and unpaid status

--- apps/web/src/app/(member)/user/requests/_components/request-card.tsx
+++ apps/web/src/app/(member)/user/requests/_components/request-card.tsx
@@ -5,23 +5,49 @@ import {
   CardHeader,
   CardTitle,
 } from "@web/src/components/ui/card"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@web/src/components/ui/tooltip"
 import { RequestSchema } from "api-contract/types"
 import { format } from "date-fns"
+import { CircleCheck, CircleDot } from "lucide-react"
 import { z } from "zod"
 
 interface RequestCardProps {
   request: z.infer<typeof RequestSchema>
   index: number
+  paid?: boolean
 }
 
-const RequestCard: React.FC<RequestCardProps> = ({ request, index }) => {
+const RequestCard: React.FC<RequestCardProps> = ({ request, index, paid }) => {
   return (
     <Link href={`/user/requests/request/${request.id}`} className="w-full">
       <Card className="w-full">
         <CardHeader>
           <CardTitle className="flex items-center justify-between text-lg">
             <div># {index + 1}</div>
-            <div>NRs {request.price}</div>
+            <div className="flex items-center justify-center gap-4">
+              {paid !== undefined && (
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger type="button">
+                      {paid ? (
+                        <CircleCheck className="size-5" />
+                      ) : (
+                        <CircleDot className="size-5" />
+                      )}
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>{paid ? "Paid" : "Pending Payment"}</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              )}
+              <div>NRs {request.price}</div>
+            </div>
           </CardTitle>
           <div>
             <dl className="space-y-2">
